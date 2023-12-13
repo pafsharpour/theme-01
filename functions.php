ccc267c1dccfcf9a586ce7b79271033a701cb757
paafsharpour <afsharpour.pariya@youremail.com>
2023-12-13 08:33:37 +0330

    add hook to add menu item in apreance

--- functions.php
+++ functions.php
@@ -9,3 +9,15 @@ function enqueue_custom_styles_and_scripts() {
     wp_enqueue_script('custom-script', get_template_directory_uri() . '/js/index.js', array(), '1.0', true);
 }
 add_action('wp_enqueue_scripts', 'enqueue_custom_styles_and_scripts');
+
+
+// Register menus
+function register_custom_menus() {
+    register_nav_menus(
+        array(
+            'header-menu' => __('Header Menu'),
+            'footer-menu' => __('Footer Menu')
+        )
+    );
+}
+add_action('init', 'register_custom_menus');
